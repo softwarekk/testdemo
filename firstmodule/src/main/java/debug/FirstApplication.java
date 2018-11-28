6a235eb4f3658471fc03e1d7cd9df66a44956e05
闫永凯 <youngyan@beile.com>
2018-11-28 10:14:44 +0800

    first commit

---
+++ firstmodule/src/main/java/debug/FirstApplication.java
@@ -0,0 +1,17 @@
+package debug;
+
+import android.app.Application;
+
+
+/**
+ * Created by dxx on 2017/11/15.
+ * 组件化编译的时候才生效
+ */
+
+public class FirstApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
